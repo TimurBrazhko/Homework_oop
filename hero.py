9fbed54122ca2fc0c578f231b7ffc571d01a5f85
TimurBrazhko <timurbrazhko@gmail.com>
2024-07-05 21:36:26 +0600

    homework_2_fin

--- hero.py
+++ hero.py
@@ -1,44 +1,70 @@
-class Superhero:
+class SuperHero:
 
     people = 'people'
 
-    def __init__(self, name, nickname, superpower, health_point, catchphrase):
+    def __init__(self, name, nickname, superpower, healthpoints, catchphrase):
+
         self.name = name
         self.nickname = nickname
         self.superpower = superpower
-        self.health_point = health_point
+        self.healthpoints = healthpoints
         self.catchphrase = catchphrase
 
     def nameprint(self):
-        return f'name: {self.name}'
+        return f'name of hero:{self.name}'
 
-    def health_point_x2(self):
-        return f'new health points: {self.health_point * 2}'
+    def healthpoint_sqare(self):
+        return self.healthpoints * 2
 
     def __str__(self):
-        return f'nickname:{self.nickname}; superpower: {self.superpower}; health points: {self.health_point};'
+        return f'nickname:{self.nickname}\nsuperpower:{self.superpower}\n{self.healthpoints}'
 
     def __len__(self):
         return len(self.catchphrase)
 
 
-class AirHero(Superhero):
+superman = SuperHero('Ken', 'Super-Man', 'better than normal person', 1000, 'Hello there')
 
-    people = 'people'
+print(superman.nameprint())
+print(superman.healthpoint_sqare())
+print(superman.__str__())
+print(superman.__len__())
 
-    def __init__(self, name, nickname, superpower, health_point, catchphrase, fly=False):
-        Superhero.__init__(self, name, nickname, superpower, health_point, catchphrase)
+
+class EarthHero(SuperHero):
+
+    def __init__(self, name, nickname, superpower, healthpoints, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, healthpoints, catchphrase)
+        self.damage = damage
         self.fly = fly
 
-    def health_point_x2(self):
-        return f'new health points: {self.health_point ** 2}'
+    def healthpoint_sqare(self):
+        return f'{self.fly != True}, {self.healthpoints ** 2}'
+
+    def true_phrase(self):
+        return f'True\n'
 
 
 
+flash = EarthHero('Flash', 'Flash', 'fast as hell', 300, 'hello dummies', 100)
+print(flash.healthpoint_sqare())
+print(flash.true_phrase())
 
-timur = Superhero('timur', 'Gngng', 'wowosurprice', 100, 'yaaaay')
 
-print(timur.nameprint())
-print(timur.__len__())
-print(timur.__str__())
-print(timur.health_point_x2())
+class Villian(EarthHero):
+
+    people = 'monster'
+
+    def __init__(self, name, nickname, superpower, healthpoints, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, healthpoints, catchphrase, damage, fly=False)
+        pass
+
+    def gen_x(self): ...
+
+    def crit(self):
+        return f'CRITICAL DAMAGE:{self.damage ** 2}'
+
+
+michle = Villian('Michel', 'Michel Maiers', 'immortal', 100000000, 'None', 1000)
+
+print(Villian.crit(flash))
